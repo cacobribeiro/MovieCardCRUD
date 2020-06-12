dba22ca5f50d8c21bad3bf725b1f9623385835d3
cacobribeiro <cacobribeiro@gmail.com>
2020-06-12 14:34:29 -0300

    DidMount implementado com função de API, iniciando Loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import MovieList from './pages/MovieList';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>Movie Card Library CRUD</div>
+      <MovieList />
     </BrowserRouter>
   );
 }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -4,14 +4,31 @@ import MovieCard from '../components/MovieCard';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: '',
+      loading: true,
+    };
+  }
+
+  async moviesApi() {
+    this.setState({ movies: await movieAPI.getMovies() });
+    this.setState({ loading: false });
+  }
+
+  componentDidMount() {
+    this.moviesApi();
+  }
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {movies.map((movie) => (
+          <MovieCard key={movie.title} movie={movie} />
+        ))}
       </div>
     );
   }
