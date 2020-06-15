08e833453a0457176c9796a518670d1709257aa3
cacobribeiro <cacobribeiro@gmail.com>
2020-06-15 19:46:36 -0300

    Tentando corrigir CC

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -10,8 +10,7 @@ class EditMovie extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(updatedMovie) {
-  }
+  handleSubmit(updatedMovie) {}
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import * as movieAPI from '../services/movieAPI';
-import { Loading } from '../components';
 import { Link } from 'react-router-dom';
+import { Loading } from '../components';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieDetails extends Component {
   constructor(props) {
@@ -12,12 +12,10 @@ class MovieDetails extends Component {
       movie: '',
     };
   }
-  componentDidMount() {
-    const { id } = this.props.match.params;
-    this.setState({ movieDetail: this.movieGet(id) });
-  }
+  componentDidMount() {}
 
   async movieGet(id) {
+    const { id } = this.props.match.params;
     this.setState({ movie: await movieAPI.getMovie(id) });
     this.setState({ loading: false });
   }
@@ -25,22 +23,24 @@ class MovieDetails extends Component {
   render() {
     if (this.state.loading === true) {
       return <Loading />;
-    } else {
-      const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
-      return (
-        <div data-testid="movie-details">
-          <img alt="Movie Cover" src={`../${imagePath}`} />
-          <p>{`Título: ${title}`}</p>
-          <p>{`Subtítulo: ${subtitle}`}</p>
-          <p>{`Sinopse: ${storyline}`}</p>
-          <p>{`Gênero: ${genre}`}</p>
-          <p>{`Avaliação: ${rating}`}</p>
-          <Link to={`/movies/${this.state.movie.id}/edit`}>EDITAR</Link>
-          <Link to="/">VOLTAR</Link>
-        </div>
-      );
     }
+    const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
+    return (
+      <div data-testid="movie-details">
+        <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Título: ${title}`}</p>
+        <p>{`Subtítulo: ${subtitle}`}</p>
+        <p>{`Sinopse: ${storyline}`}</p>
+        <p>{`Gênero: ${genre}`}</p>
+        <p>{`Avaliação: ${rating}`}</p>
+        <Link to={`/movies/${this.state.movie.id}/edit`}>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
+      </div>
+    );
   }
 }
 
+MovieCard.propTypes = {
+  id: PropTypes.number,
+};
 export default MovieDetails;
