148e411ebf406387d18ec2e6fa6ea265239fe2e7
Caio Ribeiro <cacoribeiro@pop-os.localdomain>
2020-09-09 11:37:29 -0300

    Updates

---
+++ precache-manifest.ebab6be8a9fb31c6a86674d5c4e70492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b3fe8c39213b10f49f994fdd0458ab",
+    "url": "/MovieCardCRUD/index.html"
+  },
+  {
+    "revision": "0aa273878ef4413231a7",
+    "url": "/MovieCardCRUD/static/css/main.83c4484b.chunk.css"
+  },
+  {
+    "revision": "a88852aa608d31329a79",
+    "url": "/MovieCardCRUD/static/js/2.179e996c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MovieCardCRUD/static/js/2.179e996c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aa273878ef4413231a7",
+    "url": "/MovieCardCRUD/static/js/main.8435f606.chunk.js"
+  },
+  {
+    "revision": "a9da2b88cf4ef85f7b25",
+    "url": "/MovieCardCRUD/static/js/runtime-main.9a30f0f4.js"
+  }
+]);
